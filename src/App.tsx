d86f1db6cb49b4d679c76f82f43dce8a1b00fe24
yeol0324 <yeol0324@gmail.com>
2024-05-21 14:39:18 +0900

    test push

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import { Suspense, useState, useTransition } from "react";
-import LottiePage from "./lottie/index.tsx";
+import LottiePage from "./animations/lottie/index.tsx";
 import Layout from "./Layout.tsx";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 
 export default function App() {
+  const navigate = useNavigate();
   return (
     <Suspense fallback={<BigSpinner />}>
       <Layout isPending={true}>
         <Routes>
-          <Route path="/" element={<div>this is animation project</div>} />
+          <Route
+            path="/"
+            element={
+              <div>
+                this is interactive-ux project
+                <button onClick={() => navigate("/lottie")}>lottie</button>
+              </div>
+            }
+          />
           <Route path="/lottie" element={<LottiePage />} />
         </Routes>
       </Layout>
---
+++ src/graphics/three/index.tsx
@@ -0,0 +1,19 @@
+import { Suspense } from "react";
+
+export default function ArtistPage({}) {
+  return (
+    <>
+      <div>lottielottielottielottie</div>
+    </>
+  );
+}
+
+function AlbumsGlimmer() {
+  return (
+    <div className="glimmer-panel">
+      <div className="glimmer-line" />
+      <div className="glimmer-line" />
+      <div className="glimmer-line" />
+    </div>
+  );
+}
